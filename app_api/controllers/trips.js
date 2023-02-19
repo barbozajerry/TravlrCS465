8fd01733d85834c62be5878997723fcca7e37130
barbozajerry <jerry.barbozaleon@gmail.com>
2023-02-19 08:26:18 -0500

    module 6

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -23,7 +23,7 @@ const tripsList = async (req, res) => {
 };
 
 // GET: /trips:/tripCode - returns single trip
-const tripsFindByCode = async (req, res) => {
+const tripsFindCode = async (req, res) => {
     Model
         .find({"code": req.params.tripCode })
         .exec((err, trip) => {
@@ -36,6 +36,7 @@ const tripsFindByCode = async (req, res) => {
                         .status(404)
                         .json(err);
             } else {
+                //console.log('tripsFindCode returning:\n' + trip);
                 return res
                         .status(200)
                         .json(trip);
@@ -43,7 +44,75 @@ const tripsFindByCode = async (req, res) => {
         });
 };
 
+const tripsAddTrip = async (req, res) => {
+    model
+    .create({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    },
+    (err, trip) => {
+        if (err) {
+            return res
+                .status(400)
+                .json(err);
+        } else {
+            return res
+                .status(201)
+                .json(trip);
+        }
+    
+ 
+});
+}
+
+const tripsUpdateTrip = async (req, res) => {
+    console.log(req.body);
+    model
+    Trip
+    .findOneAndUpdate({ 'code': req.params.tripCode }, {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+        }, { new: true })
+        .then(trip => {
+        if (!trip) {
+        return res
+        .status(404)
+       .send({
+        message: "Trip not found with code "
+       + req.params.tripCode
+        });
+        }
+        res.send(trip);
+        }).catch(err => {
+        if (err.kind === 'ObjectId') {
+        return res
+        .status(404)
+        .send({
+        message: "Trip not found with code "
+       + req.params.tripCode
+        });
+        }
+        return res
+        .status(500) // server error
+        .json(err);
+        });
+       }
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
