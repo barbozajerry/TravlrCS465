6696abd18193aa6ceff7e0ec1e4661e86196d10a
barbozajerry <jerry.barbozaleon@gmail.com>
2023-02-23 04:54:18 -0500

    module 7 update04

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-router.module';
@@ -11,8 +12,8 @@ import { TripDataService } from './services/trip-data.service';
 import { AddTripComponent } from './add-trip/add-trip.component';
 import { EditTripComponent } from './edit-trip/edit-trip.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -22,12 +23,14 @@ import { HomeComponent } from './home/home.component';
     AddTripComponent,
     EditTripComponent,
     NavbarComponent,
-    LoginComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule, 
+    BrowserModule,
+    HttpClientModule,
     HttpModule,
+    FormsModule,
     ReactiveFormsModule,
     AppRoutingModule
   ],
